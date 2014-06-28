39fa62ebde8145f0fa7af822d40e9e717ec68d51
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-06-28 15:49:30 -0700

    refactored the error print fn

--- sepharad.js
+++ sepharad.js
@@ -1,14 +1,13 @@
-var validate = require("./lib/validate"),
+var validator = require("./lib/validator"),
 	example = require("./lib/example");
 
 var sepharad = module.exports = function sepharad (options) {
 
 	options = options || {};
 
-	var errors = validate(options);
-
+	var errors = validator.validate(options);
 	if (errors.length) {
-		errors.forEach(console["dir"]);
+		validator.printErrors(errors);
 		example.print();
 		
 		return;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,12 @@
 var sepharad = require("../sepharad");
 
+// test 1
 sepharad();
+
+// test 2
+// var example = {
+// 	url : "",
+// 	path: "lib/",
+// 	output: "report.html"
+// };
+// sepharad(example);
